e1257b993596eda9fb34f6e57787f5d3a70f2b53
curtlawson-WCHS <119456904+curtlawson-WCHS@users.noreply.github.com>
2024-03-12 09:44:38 -0400

    Update setup.sh
    updated "speedtest-net" to just "speedtest"

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo apt update
-sudo apt install toilet figlet lolcat cmatrix cbonsai cmatrix curl speedtest-net nmon htop curl -y
+sudo apt install toilet figlet lolcat cmatrix cbonsai cmatrix curl speedtest nmon htop curl -y
 sudo apt full-upgrade -y
