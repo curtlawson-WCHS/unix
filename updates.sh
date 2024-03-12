7959e9fee00684621f044c444f5ceddf88b35a00
curtlawson-WCHS <119456904+curtlawson-WCHS@users.noreply.github.com>
2024-03-12 09:36:10 -0400

    Create updates.sh

---
+++ updates.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+printf("\n")
+/usr/bin/figlet "Checking for updates..." | /usr/games/cowsay -f tux -n | lolcat -a -s 400
+sudo apt update
+
+/usr/bin/figlet "Installing updates..." | /usr/games/cowsay -f tux -n | lolcat -a -s 400
+sudo apt upgrade -y
+
+/usr/bin/figlet "Cleaning up (1/2)..." | /usr/games/cowsay -f tux -n | lolcat -a -s 400
+sudo apt autoremove -y
+
+/usr/bin/figlet "Running upgrade..." | /usr/games/cowsay -f tux -n | lolcat -a -s 400
+sudo apt full-upgrade -y
+
+/usr/bin/figlet "Cleaning up (2/2)..." | /usr/games/cowsay -f tux -n | lolcat -a -s 400
+sudo apt autoremove -y
+
+printf("\n")
+/usr/bin/neofetch
+exit
