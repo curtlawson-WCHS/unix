1b7e9e3d40b08d75adb5674feb916a4b9cdd7bbe
curtlawson-WCHS <119456904+curtlawson-WCHS@users.noreply.github.com>
2024-09-24 12:44:55 +0000

    Created a new update file with a dragon!

---
+++ updates_dragon.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+clear
+echo ""
+/usr/bin/figlet "Checking for updates..." | /usr/games/cowsay -f dragon -n | lolcat -a -s 400
+sudo apt update
+
+/usr/bin/figlet "Installing updates..." | /usr/games/cowsay -f dragon -n | lolcat -a -s 400
+sudo apt upgrade -y
+
+/usr/bin/figlet "Cleaning up (1/2)..." | /usr/games/cowsay -f dragon -n | lolcat -a -s 400
+sudo apt autoremove -y
+
+/usr/bin/figlet "Running upgrade..." | /usr/games/cowsay -f dragon -n | lolcat -a -s 400
+sudo apt full-upgrade -y
+
+/usr/bin/figlet "Cleaning up (2/2)..." | /usr/games/cowsay -f dragon -n | lolcat -a -s 400
+sudo apt autoremove -y
+
+echo ""
+/usr/bin/neofetch
+exit
