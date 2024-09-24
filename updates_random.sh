61e5e1634e7b09bd49cce22a1fa47428a49459a2
curtlawson-WCHS <119456904+curtlawson-WCHS@users.noreply.github.com>
2024-09-24 09:32:19 -0400

    Update updates_random.sh
    Updated the variable to actually work, LOL

--- updates_random.sh
+++ updates_random.sh
@@ -4,19 +4,19 @@ selected_file = $(ls "$directory" | shuf -n 1)
 
 clear
 echo ""
-/usr/bin/figlet "Checking for updates..." | /usr/games/cowsay -f selected_file -n | lolcat -a -s 400
+/usr/bin/figlet "Checking for updates..." | /usr/games/cowsay -f $selected_file -n | lolcat -a -s 400
 sudo apt update
 
-/usr/bin/figlet "Installing updates..." | /usr/games/cowsay -f selected_file -n | lolcat -a -s 400
+/usr/bin/figlet "Installing updates..." | /usr/games/cowsay -f $selected_file -n | lolcat -a -s 400
 sudo apt upgrade -y
 
-/usr/bin/figlet "Cleaning up (1/2)..." | /usr/games/cowsay -f selected_file -n | lolcat -a -s 400
+/usr/bin/figlet "Cleaning up (1/2)..." | /usr/games/cowsay -f $selected_file -n | lolcat -a -s 400
 sudo apt autoremove -y
 
-/usr/bin/figlet "Running upgrade..." | /usr/games/cowsay -f selected_file -n | lolcat -a -s 400
+/usr/bin/figlet "Running upgrade..." | /usr/games/cowsay -f $selected_file -n | lolcat -a -s 400
 sudo apt full-upgrade -y
 
-/usr/bin/figlet "Cleaning up (2/2)..." | /usr/games/cowsay -f selected_file -n | lolcat -a -s 400
+/usr/bin/figlet "Cleaning up (2/2)..." | /usr/games/cowsay -f $selected_file -n | lolcat -a -s 400
 sudo apt autoremove -y
 
 echo ""
