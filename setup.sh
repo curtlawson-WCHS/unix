4c37081b8b6c8ee112ccc5c60628e0a4ac9c6013
Curt Lawson <119456904+curtlawson-WCHS@users.noreply.github.com>
2024-12-13 15:43:46 -0500

    Update setup.sh
    Updates that will work on any distro with "apt"

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 sudo apt update
-sudo apt install wget toilet figlet lolcat cmatrix cbonsai curl nmon htop curl neofetch -y
+sudo apt install wget -y
+sudo apt install toilet -y
+sudo apt install figlet -y
+sudo apt install lolcat -y
+sudo apt install cmatrix -y
+sudo apt install cbonsai -y
+sudo apt install curl -y
+sudo apt install neofetch -y
+sudo apt install nmon -y
+sudo apt install htop -y
+# sudo apt install wget toilet figlet lolcat cmatrix cbonsai curl nmon htop curl neofetch -y
 sudo apt full-upgrade -y
